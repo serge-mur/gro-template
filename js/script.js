1175fef6c5de8bd08cbb06a0a93e1cc40e72f493
Serge <serge-mur@yandex.ru>
2025-05-15 15:31:47 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
     console.log('document ready');
+
+    const insideSlider = new Swiper('.inside-slider', {
+        slidesPerView: 4.25,
+        spaceBetween: 8,
+    });
 });
