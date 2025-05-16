b67e0445f7fead271806adc2b7507e1e63dd7388
Serge <serge-mur@yandex.ru>
2025-05-16 16:01:12 +0300

    imports

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     
     console.log('document ready');
 
+    const burgerBtn = document.querySelector('.header__burger');
+    const mobMenu = document.querySelector('.header__menu');
+    const mobLogin = document.querySelector('.header__login');
+    burgerBtn.addEventListener('click', () => {
+        mobLogin.classList.toggle('header__login_show');
+        mobMenu.classList.toggle('header__menu_open');
+    });
+
     const insideSlider = new Swiper('.inside-slider', {
         slidesPerView: 4.25,
         spaceBetween: 8,
